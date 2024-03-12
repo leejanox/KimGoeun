ecf3e530bf1d2884ccbfab3456a6c4057664fe99
leejanox <162652877+leejanox@users.noreply.github.com>
2024-03-12 16:52:05 +0900

    Add files via upload

---
+++ 240312database.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello")
+print(3)
+
+print(53000 * 10)
+
+print("파이썬 수업중")
+print("2024-03-12")
+# 샵 이후에 쓴 글은 실행이 되지 않는다. 주석을 붙일땐 샵을 붙여서 설명을 쓴다.
+print(1,"\n",2,"\n")
+#1과 \n 은 타입이 달라 ,로 구분해줘야 함.
+print("python1\npython2\npython3")
+print("python1\tpython2\tpython3")
+#"\n","\t" 개행문자, 탭은 모두 "" 안에 들어가 있어야 역할을 함
+print("123\t0.00\t15000")
+print("1\t1234\t5")
+print("11\t0.22\t15.00")
+#탭을써서 출력을 하면 출력시 라인이 맞춰져서 깔끔해짐.
+print('*'*80)
+#*을 80개 다 찍는게 아니다
