bcedf220dbc70c9f6efd5f3286d5087fd3ad3947
leejanox <162652877+leejanox@users.noreply.github.com>
2024-03-15 11:41:32 +0900

    Add files via upload

---
+++ 3-1.py
@@ -0,0 +1,68 @@
+#3-1
+'''
+print("hello world!")
+print("hello  world!")
+print("hello" + "world")
+print("hello" + " " + "world")
+'''
+
+#3-2
+'''
+print("오늘은 파이썬 챌린지 3일차 입니다)
+print('디버깅 "+" 연습중입니다')
+ print('내일은("금요일" + "입니다)')
+print(("그래서 챌린지가 없습니다"))
+'''
+'''
+print("오늘은 파이썬 챌린지 3일차 입니다")
+print('디버깅 연습중입니다')
+print('내일은 금요일 입니다')
+print("그래서 챌린지가 없습니다"
+'''
+
+#3-3
+'''
+print("나는" + input("당신의 이름은 무엇입니까?") + "입니다")
+'''
+
+#3-4
+'''
+print(len(input("당신의 이름은 무엇입니까?")))
+'''
+
+#3-5
+
+# name= input("당신의 이름은 무엇입니까?")
+# print(name)
+
+#3-6
+
+# name=input("당신의 이름은 무엇입니까?")
+# length=len(name)
+# print(length)
+
+#3-6(2)
+
+# a="dog"
+# b="cat"
+
+# c=a
+# a=b
+# b=c
+
+# print(" a = " + a)
+# print(" b = " + b)
+
+#3-7
+#3일차 종합 챌린지 퀴즈
+
+# print("내가 좋아하는 영화는 " + input("당신이 좋아하는 영화는 무엇입니까?") + "이고 주인공은 " + input("주인공은 누구입니까?") + "입니다.")
+
+a=input("내가 좋아하는 영화는 무엇입니까?")
+b=input("주인공은 누구입니까?")
+
+c=int(a)
+d=int(b)
+
+print(c+d)
+print("내가 좋아하는 영화는 "+ a + "이고, 주인공은 "+ b + "입니다.")
